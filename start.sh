f1281d12bc25ac9edf3a9c3063cd8b2a47eef5a9
je <je@JM>
2019-09-30 02:22:20 +0800

    ..

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ BUILD_ID=dontKillMe
 
 #此处放入shell脚本或者shell命令
 
-nohup java -jar target/HttpServer-1.0-SNAPSHOT.jar &>out.txt &
+java -jar target/HttpServer-1.0-SNAPSHOT.jar &>out.txt
 
 
 BUILD_ID=$OLD_BUILD_ID
