78c8e0fa834794b0995359033b9c6de28765a525
je <je@JM>
2019-09-30 02:18:48 +0800

    ..

--- start.sh
+++ start.sh
@@ -1,2 +1,15 @@
 
-nohup java -Dhudson.util.ProcessTree.disable=true -jar target/HttpServer-1.0-SNAPSHOT.jar &>out.txt &
+OLD_BUILD_ID=$BUILD_ID
+echo $OLD_BUILD_ID
+BUILD_ID=dontKillMe
+
+#此处放入shell脚本或者shell命令
+
+nohup java -jar target/HttpServer-1.0-SNAPSHOT.jar &>out.txt &
+
+
+BUILD_ID=$OLD_BUILD_ID
+echo $BUILD_ID
+
+
+
