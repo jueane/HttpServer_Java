695a28f3e9b68dd1ec94ca3e4372212154c4b577
je <je@jm.com>
2019-11-09 19:55:23 +0800

    ..

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #JENKINS_NODE_COOKIE=dontKillMe
 BUILD_ID=dontkillme
-nohup java -jar target/HttpServer-1.0-SNAPSHOT.jar &>out.txt &
+sudo nohup java -jar target/HttpServer-1.0-SNAPSHOT.jar &>out.txt &
